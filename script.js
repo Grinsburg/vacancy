4a5b46960466fbedf181241b904588b8e00939d1
Grinsburg <smlynchik@mail.ru>
2019-11-20 13:42:32 +0300

    fetch data from page

--- script.js
+++ script.js
@@ -1,62 +1,53 @@
-// let myInit = {
-//     method: 'GET',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     mode: 'no-cors',
-//     cache: 'default',
-//     body: JSON.stringify()
-// }
-
-// let myRequest = new Request("https://jobs.github.com/positions.json", myInit);
-let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-    targetUrl = 'https://jobs.github.com/positions.json'
-console.log(proxyUrl + targetUrl);
-
-fetch(proxyUrl + targetUrl)
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    })
-
-
-// fetch("https://jobs.github.com/positions.json?description=python", { mode: "no-cors" })
-//     .then(
-//         function(response) {
-//             if (response.status = 200) {
-//                 console.log('Loo!=ks like there was a problem. Status Code: ' +
-//                     response.status);
-//                 return;
-//             }
-
-//             // Examine the text in the response
-//             response.json().then(function(data) {
-//                 console.log(data);
-//             });
-//         }
-//     )
-//     .catch(function(err) {
-//         console.log('Fetch Error :-S', err);
-//     });
-
-// fetch('https://jobs.github.com/positions.json', {
-//     method: 'GET',
-//     mode: 'no-cors',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     }
-// }).then(function(response) {
-//     console.log(response);
-//     return response.json();
-
-// }).catch(function(err) {
-//     console.log(err)
-// });
-
-
 let idBtn = document.getElementById('search');
-idBtn.addEventListener('click', function() {
+let lang = document.getElementById('firstInput');
+let city = document.getElementById('secondInput');
+let output = document.getElementById('output');
+let data = [];
 
+
+idBtn.addEventListener('click', (e) => {
+    let targetUrl = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${lang.value}&location=${city.value}`;
+    console.log(targetUrl);
+
+    fetch(targetUrl)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(json => {
+            json.forEach(elem => { createCard(elem.title, elem.company, elem.type, elem.location, elem.url) })
+        })
+    console.log(lang.value);
+    console.log(city.value);
+
+    const createCard = (title, company, type, location, url) => {
+        let card = document.createElement('div');
+        let header = document.createElement('div');
+        let body = document.createElement('div');
+        let footer = document.createElement('div');
+        let h5 = document.createElement('h5');
+        let p = document.createElement('p');
+        let link = document.createElement('a');
+
+        card.classList = 'card text-start m-2';
+        header.innerHTML = `${location}`;
+        header.classList = 'card-header';
+        body.classList = 'card-body';
+        h5.classList = 'card-title';
+        p.classList = 'card-text';
+        p.textContent = `${company}`;
+        h5.textContent = `${title}`;
+        link.href = `${url}`;
+        link.classList = 'btn btn-primary';
+        link.textContent = 'Подробнее';
+        body.appendChild(h5)
+        body.appendChild(p)
+        body.appendChild(link);
+        footer.classList = 'card-footer text-muted'
+        footer.textContent = `${type}`
+        output.appendChild(card);
+        card.appendChild(header);
+        card.appendChild(body);
+        card.appendChild(footer);
+
+    }
 })
